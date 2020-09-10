bae6f6f563296ce23c9d070cd42bc64ae3ff9c26
Dan Wellman <gitkraken@danwellman.com>
2020-09-09 22:01:26 -0700

    start framework

--- Client.lua
+++ Client.lua
@@ -1,1 +1,15 @@
---
+local addonName, envTable = ...
+setmetatable(envTable, {__index = _G})
+setfenv(1, envTable)
+
+local ClientFrame = CreateFrame("Frame")
+ClientFrame:SetWidth(800)
+ClientFrame:SetHeight(600)
+ClientFrame:SetPoint("CENTER")
+
+local Background = ClientFrame:CreateTexture(nil, "BACKGROUND", -8)
+Background:SetColorTexture(0, 0, 0, 1)
+Background:SetAllPoints(ClientFrame)
+
+local RenderFrame = CreateFrame("Frame", nil, ClientFrame)
+RenderFrame:SetClipsChildren(true)
--- Entities/Player.lua
+++ Entities/Player.lua
@@ -1,1 +1,9 @@
---
+local addonName, envTable = ...
+setmetatable(envTable, {__index = _G})
+setfenv(1, envTable)
+
+
+GameObjectMixin = {}
+
+function GameObjectMixin:SetLocation(location)
+end
